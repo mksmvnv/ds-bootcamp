951613f19da92056d6be4820d6cc39d05295d61b
Maksim <mksmvnv@icloud.com>
2024-12-29 15:40:28 +0300

    Implement dict sorting by country numbers and alphabet

---
+++ src/day01/ex06/dict_sorter.py
@@ -0,0 +1,87 @@
+def get_tuple_list() -> list:
+    """Функция возвращает список кортежей.
+
+    Returns:
+        list: Список кортежей.
+    """
+
+    tuple_list = [
+        ("Russia", "25"),
+        ("France", "132"),
+        ("Germany", "132"),
+        ("Spain", "178"),
+        ("Italy", "162"),
+        ("Portugal", "17"),
+        ("Finland", "3"),
+        ("Hungary", "2"),
+        ("The Netherlands", "28"),
+        ("The USA", "610"),
+        ("The United Kingdom", "95"),
+        ("China", "83"),
+        ("Iran", "76"),
+        ("Turkey", "65"),
+        ("Belgium", "34"),
+        ("Canada", "28"),
+        ("Switzerland", "26"),
+        ("Brazil", "25"),
+        ("Austria", "14"),
+        ("Israel", "12"),
+    ]
+
+    return tuple_list
+
+
+def convert_to_dict(tuple_list: list) -> dict:
+    """Функция преобразует список кортежей в словарь, где ключ - это число, а значение - список стран.
+
+    Args:
+        tuple_list (list): Список кортежей.
+
+    Returns:
+        dict: Словарь, где ключ - это число, а значение - список стран.
+    """
+
+    converted_dict = {}
+
+    # Преобразуем список кортежей в словарь, где ключом будет число,
+    # А значением список стран с этим числом
+    for country, num in tuple_list:
+        if country not in converted_dict:
+            converted_dict[country] = []
+        converted_dict[country].append(num)
+
+    return converted_dict
+
+
+def sort_dict(converted_dict: dict) -> dict:
+    """
+    Функция сортирует страны по убыванию номеров.
+    Если номера одинаковые, применяется сортировка по алфавиту.
+
+    Args:
+        converted_dict (dict): Словарь, где ключ - это число, а значение - список стран.
+
+    Returns:
+        dict: Словарь, где ключ - это число, а значение - список стран.
+    """
+
+    sorted_dict = dict(
+        sorted(converted_dict.items(), key=lambda x: (-int(x[1][0]), x[0]))
+    )
+
+    return sorted_dict
+
+
+def main() -> None:
+    """Основная функция программы."""
+
+    tuple_list = get_tuple_list()
+    converted_dict = convert_to_dict(tuple_list)
+    sorted_dict = sort_dict(converted_dict)
+
+    for k, _ in sorted_dict.items():
+        print(k)
+
+
+if __name__ == "__main__":
+    main()
